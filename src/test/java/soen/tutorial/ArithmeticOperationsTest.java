9384f377b0ae97eb921700eda82784412d4d00f3
Shanvin Luo <shanvinluo@gmail.com>
2025-04-02 16:49:30 -0400

    commit

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,6 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
@@ -14,7 +15,7 @@ public class ArithmeticOperationsTest {
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-        fail('Must Fail')
+        fail("Must Fail");
 	}
     // @Test
     // public void testSub()
